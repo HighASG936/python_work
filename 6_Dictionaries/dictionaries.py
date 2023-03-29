75c1ef41e02c33a2fb32ad057b7b036b4fc4b3f1
Aurelio Siordia <lz_rock@hotmail.com>
2023-03-28 21:50:34 -0600

    Dictionaries

---
+++ 6_Dictionaries/dictionaries.py
@@ -0,0 +1,39 @@
+#Name: Aurelio Siordia
+#Date: 28/03/23
+
+#6-1 Person
+Person={'first_name': 'Stephany',
+		'last_name':  'Harrison',
+		'age': 26,
+		'city': 'New York'
+		}
+print(f"Complete Name: {Person['first_name']} {Person['last_name']}")
+print(f"Age: {Person['age']}")
+print(f"City: {Person['city']}\n")
+
+#6-2 Favorite Numbers
+favorite_numbers={'Catherine':8,
+				  'Louis': 10,
+				  'Carl': 7,
+				  'William': 2,
+				  'Max': 4,
+                 }
+
+print(favorite_numbers['Catherine'])
+print(favorite_numbers['Louis'])
+print(favorite_numbers['Carl'])
+print(favorite_numbers['William'])
+print(f"{favorite_numbers['Max']}\n")
+
+glossary={'Principio': 'Lo que está al inicio de algo',
+			'Imitar': 'Ejecutar algo a ejemplo o semejanza de otra cosa',
+			'Cigarrillos': ' Cigarro pequeño de picadura envuelta en un papel de fumar.',
+			'Oro': 'Elemento químico metálico, de núm. atóm. 79, de color amarillo brillante, el más dúctil y maleable de los metales, muy buen conductor del calor y la electricidad, escaso en la corteza terrestre, donde se encuentra nativo y muy disperso, y que se usa en joyería, en la fabricación de monedas y, aleado con platino o paladio, en odontología. (Símb. Au).',
+			'Negocio': 'Ocupación, quehacer o trabajo.'
+		   }
+print(f"Principio: {glossary['Principio']}")
+print(f"Imitar: {glossary['Imitar']}")
+print(f"Cigarrillos: {glossary['Cigarrillos']}")
+print(f"Oro: {glossary['Oro']}")
+print(f"Negocio: {glossary['Negocio']}")
+
