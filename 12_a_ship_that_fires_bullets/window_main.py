d9a5b705ae2cec4f4044250d4d537a0ede71f7d4
Aurelio Siordia <lz_rock@hotmail.com>
2023-04-13 12:18:51 -0600

    piloting the ship

--- 12_a_ship_that_fires_bullets/window_main.py
+++ 12_a_ship_that_fires_bullets/window_main.py
@@ -4,6 +4,7 @@
 import sys
 import pygame
 from character_game import Character
+from settings import Settings
 
 #12-1 Blue Sky
 class WindowMainGame: 
@@ -12,20 +13,59 @@ class WindowMainGame:
     def __init__(self):
         """ """
         pygame.init()
+        self.settings = Settings()
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
-        self.character = Character(self)
+        self.character_game = Character(self)
 
+    def _check_events(self):
+        """Watch for keyboard and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)    
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypress."""
+        if event.key == pygame.K_RIGHT:
+            self.character_game.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.character_game.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.character_game.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.character_game.moving_down = True            
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Response to key releases."""
+        print(event.key)
+        if event.key == pygame.K_RIGHT:
+            self.character_game.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.character_game.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.character_game.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.character_game.moving_down = False 
+
+    def _update_screen(self):        
+        """Update images on the screen, and flip to the new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.character_game.blitme()
+        pygame.display.flip()
+    
     def run_game(self):
         """ """
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.character_game.update()
+            self._update_screen()     
 
-            self.screen.fill((10,73,123))
-            self.character.blitme()
-            pygame.display.flip()
 
 if __name__ == '__main__':
     wa_game = WindowMainGame()
