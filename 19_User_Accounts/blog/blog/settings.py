1181da713b9072449e1d48dff570acc649a040bc
Aurelio Siordia <lz_rock@hotmail.com>
2023-05-11 22:36:33 -0600

    19-2 Blog Accounts

--- 19_User_Accounts/blog/blog/settings.py
+++ 19_User_Accounts/blog/blog/settings.py
@@ -33,8 +33,9 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #My apps.
     'blogs',
+    'users',
 
-    #Deafult django apps. 
+     #Deafult django apps. 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
